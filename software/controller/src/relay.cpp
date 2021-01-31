700bbd599b82c6b7325fd51c875395ea29bfd5b5
anno73 <anno@opossum.htu.tuwien.ac.at>
2021-01-31 21:23:40 +0100

    Search for new modules and initialize

--- software/controller/src/relay.cpp
+++ software/controller/src/relay.cpp
@@ -13,7 +13,6 @@ namespace relay
 {
 
     /**
-     * 
      * I2C
      * 
      * Default I2C pins on ESP8266
@@ -25,7 +24,18 @@ namespace relay
     const uint8_t PIN_SCL = D2;
 
     const uint8_t MAX_IIC_RETRIES = 5;
+    const uint8_t INITIAL_IIC_ADDRESS = 127; // "Factory" default IIC address of an uninitialized relay module
+    const uint8_t MAX_RELAY_MODULES = 20;    // Maximum number of supported relay modules. Mainly for sanity reasons on IIC bus scanning.
+    const uint8_t MODULE_ADDR_OFFSET = 10;   // Start here with addresses for relay modules
 
+    volatile uint8_t countSockets = 0; // Number of detected relay modules.
+
+    /**
+     * sendIicCommand
+     * 
+     * Send a single IIC command to relay module
+     * 
+     */
     uint8_t sendIicCommand(uint8_t address, uint8_t iicReg, uint8_t value, uint8_t *status, uint8_t *tries)
     {
         *status = 255;
@@ -80,8 +90,8 @@ namespace relay
         case 2:
             Serial << F("ERROR: Write to relay ") << addr << F(" Status: ") << status << F(" Try: ") << tries << endl;
             break;
-        // default:
-        //     break;
+            // default:
+            //     break;
         }
 
         return;
@@ -96,33 +106,92 @@ namespace relay
         uint8_t status = 255;
         uint8_t tries = 0;
 
-        do
+        switch (sendIicCommand(addr, iicRegister::LED, state, &status, &tries))
         {
-            Wire.beginTransmission(addr);
-            Wire.write(iicRegister::LED);
-            Wire.write(state);
-            status = Wire.endTransmission();
-
-            // Serial << F("Write to relay ") << relay << F(" Status: ") << status << F(" Try: ") << tries << endl;
-            if (status != 0)
-            {
-                tries++;
-                delay(10);
-            }
-        } while ((status != 0) && (tries <= MAX_IIC_RETRIES));
-
-        if (status == 0 && tries != 0)
-        {
+        case 1:
             Serial << F("WARNING: Write to led ") << addr << F(" Status: ") << status << F(" Try: ") << tries << endl;
-        }
-
-        if (status != 0)
-        {
+            break;
+        case 2:
             Serial << F("ERROR: Write to led ") << addr << F(" Status: ") << status << F(" Try: ") << tries << endl;
+            break;
+        default:
+            break;
         }
 
         return;
     } // switchLed
+
+    /**
+     * setIicAddress
+     * 
+     */
+    void setIicAddress(uint8_t currentAddr, uint8_t newAddr)
+    {
+        uint8_t status = 255;
+        uint8_t tries = 0;
+
+        switch (sendIicCommand(currentAddr, iicRegister::ADDR, newAddr, &status, &tries))
+        {
+        case 1:
+            Serial << F("WARNING: Write to address ") << currentAddr << F(" Status: ") << status << F(" Try: ") << tries << endl;
+            break;
+        case 2:
+            Serial << F("ERROR: Write to address ") << currentAddr << F(" Status: ") << status << F(" Try: ") << tries << endl;
+            break;
+        default:
+            break;
+        }
+
+        return;
+    } // setIicAddress
+
+    /**
+     * getModule
+     * 
+     * Read registers from relay module
+     * 
+     * 
+     */
+    uint8_t getModule(uint8_t idx, uint8_t *status, uint8_t *relay, uint8_t *led, uint8_t *addr, uint8_t *swVers)
+    {
+        uint8_t response[5];
+        uint8_t res = 0;
+
+        // if (idx > socketCnt) return 100;
+
+        // res = readIicRegs(...);
+
+        return 0;
+    } // getModule
+
+    /**
+     * probeIicBusAddr
+     * 
+     * Probe IIC bus on a given address to see if a device is present.
+     * 
+     */
+    uint8_t probeIicBusAddr(uint8_t addr)
+    {
+        Serial << F("probeIicBusAddr: ") << addr;
+
+        uint8_t status = 255;
+        uint8_t retryCount = 0;
+
+        do
+        {
+            Wire.beginTransmission(addr);
+            status = Wire.endTransmission();
+            if (status != 0)
+            {
+                retryCount++;
+                delay(10);
+            }
+        } while (status != 0 && retryCount <= MAX_IIC_RETRIES);
+
+        Serial << F(" Status: ") << status << endl;
+
+        return status;
+    } // probeIicBusAddr
 
     /**
      * San IIC bus for devices and wait forever
@@ -199,7 +268,7 @@ namespace relay
 
     void timedToggleRelayPolled(uint8_t address)
     {
-        static esp8266::polledTimeout::periodicMs tick(1000);
+        static esp8266::polledTimeout::periodicMs tick(5000);
 
         if (tick)
         {
@@ -214,18 +283,50 @@ namespace relay
     void setup(void)
     {
 
-        Serial << F("Setup relay boards") << endl;
+        Serial << F("Setup relay modules") << endl;
 
         // Setup IIC
 
         Wire.begin(PIN_SDA, PIN_SCL);
 
-        // Initialize and check for relay boards
+        // scanIicBus(115, 127);     // Scan address range on IIC bus for existing devices and loop endlessly
+        // endlessToggleRelais(125); // debugging IIC communication
+
+        // Initialize and check for relay modules
         // do we get response on factory IIC address (what to use? 127? 0 is all call)? If so we want to start a initialization process.
 
-        // scanIicBus(115, 127);
+        // Search for first free relay board address beginning at offset
+        Serial << F("Probing for modules from address ") << MODULE_ADDR_OFFSET << " to " << MODULE_ADDR_OFFSET + MAX_RELAY_MODULES << endl;
+        uint8_t freeAddr;
+        for (freeAddr = MODULE_ADDR_OFFSET; freeAddr <= MAX_RELAY_MODULES; freeAddr++)
+        {
+            uint8_t status = probeIicBusAddr(freeAddr);
+            // Status != 0 is an error, we most likely hit the first free address
+            if (status)
+                break;
+        }
 
-        // endlessToggleRelais(125);
+        Serial << F("First free IIC address: ") << freeAddr << endl;
+        countSockets = freeAddr - MODULE_ADDR_OFFSET;
+
+        // Check if we have new devices on IIC bus and free IIC addresses left
+        if (probeIicBusAddr(INITIAL_IIC_ADDRESS) == 0)
+        {
+            // Found a new device, need to initialize it with next free IIC address
+            Serial << F("Found new device at ") << INITIAL_IIC_ADDRESS << endl;
+
+            if (freeAddr)
+            {
+                Serial << F("Set new device's address to ") << freeAddr << endl;
+
+                // Set module's new IIC address
+                setIicAddress(INITIAL_IIC_ADDRESS, freeAddr);
+            }
+            else
+            {
+                Serial << F("WARNING: new device found but no free address left") << endl;
+            }
+        }
 
         return;
     } // setup
@@ -238,7 +339,7 @@ namespace relay
     {
         // endlessToggleRelaisRaw(125);
         // timedToggleRelay(125);
-        timedToggleRelayPolled(125);
+        // timedToggleRelayPolled(125);
 
         return;
     } // loop
--- software/controller/src/relay.h
+++ software/controller/src/relay.h
@@ -4,10 +4,13 @@
 
 namespace relay {
 
+extern volatile uint8_t countSockets;
 
 extern void setup();
 extern void loop();
 extern void switchRelay(uint8_t index, uint8_t state);
 extern void switchLed(uint8_t index, uint8_t state);
+
+extern uint8_t getModule(uint8_t, uint8_t *, uint8_t *, uint8_t *, uint8_t *, uint8_t *);
 
 } // namespace relay
