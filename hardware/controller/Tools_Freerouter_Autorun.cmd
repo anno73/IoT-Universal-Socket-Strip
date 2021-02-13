b6a401f18b9e6df3670deea3ed65e39be9a6f9c5
anno73 <anno@opossum.htu.tuwien.ac.at>
2021-02-13 22:31:25 +0100

    Initial version

---
+++ hardware/controller/Tools_Freerouter_Autorun.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+:: Call freerouter to auto route EDA (KiCad) board file (dsn) to Specctra Session file (ses).
+:: 
+:: https://github.com/freerouting/freerouting
+:: https://github.com/freerouting/freerouting#using-the-command-line-arguments
+
+set file=controller
+set maxPasses=-mp 100
+
+set source=-de "%CD%\%file%.dsn"
+set target=-do "%CD%\%file%.ses"
+
+"%LOCALAPPDATA%\Freerouting\Freerouting.exe" %source% %target% %maxPasses%
+
+if errorlevel 1 (
+	echo There was an error. ERRORLEVEL is %errorlevel%.
+	echo Does the source file exist?
+	pause
+	goto:eof
+)
+
+:: pause
